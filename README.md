0602356f49ed96f422bdd17235b0c5a7679b5cac
rolandoandre123 <rbardalespriale@gmail.com>
2025-01-11 15:32:16 -0500

    add : set up prime ng

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # PipesApp
+
+### Technologies
+
+### PrimeNg
+
+### STARTUP
+####  doc reference:
+[primeNG](https://v17.primeng.org/configuration)
+#### 1. execute on terminal
+```
+npm install primeicons@17
+npm install npm install primeicons
+```
+#### 2. put on styles.css
+```
+@import "primeng/resources/themes/lara-dark-indigo/theme.css";
+@import "primeng/resources/primeng.css";
+@import "primeicons/primeicons.css";
+```
+### 3. setup ripple
+```
+public ngOnInit(): void {
+  this.primengConfig.ripple = true;
+}
+```
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title: string = 'rOlaNdo bArdaLes';
+
+  constructor(private primengConfig: PrimeNGConfig) { }
+
+  public ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
+
 }
